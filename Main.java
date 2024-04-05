a2d988b33e0aa6a7557c7d0879aaf0467924ad9f
Casondra Simons <casondra.simons@smusd90.org>
2024-04-05 08:56:35 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {  
   public static void main( String args[ ] ) { 
-       Avatar cassie = new Avatar( "sea", "trident" ); 
+       Avatar cassie = new Avatar( "sea", "trident" );  //johny 
        cassie.fightBoss( );
   } 
   public static class Avatar { 
